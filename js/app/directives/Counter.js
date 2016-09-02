e7616b3979c4f5476d511efb061a754d452c2f31
agdavid <antonio.g.david@gmail.com>
2016-09-02 15:47:12 -0400

    use jQlite off() to remove event listeners if element is destroyed

--- js/app/directives/Counter.js
+++ js/app/directives/Counter.js
@@ -4,11 +4,26 @@ function Counter() {
 			'<div>',
 				'<h3>Counter</h3>',
 				'<div>Click anywhere to increment the counter!</div>',
-				'<div>Current count: {{ count }}</div>',
+				'<div>Current count: {{ some.count }}</div>',
 			'</div>'
 		].join(''),
-		controller: function ($scope) {
-			$scope.count = 0;
+		controller: function () {
+			this.count = 0;
+		},
+		//use require and controllerAs to eliminate '$scope' from controller
+		require:'counter',
+		controllerAs: 'some',
+		link: function(scope, elem, attrs, ctrl) {
+			//add listener to 'document' object to permit clicking anywhere, not just directive
+			document.addEventListener('click', function() {
+				ctrl.count += 1;
+				//call $apply to trigger re-$digest of DOM
+				scope.$apply();
+			});
+			//destroy listener before unmounted from DOM
+			scope.$on('$destroy', function() {
+				elem.off();
+			});
 		}
 	}
 }
